58482585de94a32cc154bec06655f392bf29fed0
vasily <v.mishanin@getalon.ru>
2023-08-28 11:41:23 +0600

    refactor

--- src/hooks/useOutsideClick.tsx
+++ src/hooks/useOutsideClick.tsx
@@ -5,9 +5,6 @@ import { MouseEvent, useEffect } from 'react';
  */
 export function useOutsideClick(ref: any, callback: Function) {
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
     function handleClickOutside(event: Event) {
       if (ref.current && !ref.current.contains(event.target)) {
         callback(event);
